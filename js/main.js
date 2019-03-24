feefcb3e0596140df853b1d3e399598070184cbe
natasajemojeime <natasa.kujavic@gmail.com>
2019-03-24 21:29:26 +0100

    Open i Close Modal radiiiii

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ $( function() {
   });
 });
 /* ======== modal date picker END========== */
+
+
+
+
+
+/* ======== modal open i close========== */
+$('.CTA').on('click', function(event) {
+  event.preventDefault();
+  /* Act on the event */
+  $('.modal').addClass('open');
+});
+$('.modal .close').on('click', function(event) {
+  event.preventDefault();
+  /* Act on the event */
+  $('.modal').removeClass('open');
+});
+/* ======== modal open i close END========== */
